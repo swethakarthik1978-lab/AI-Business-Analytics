93d1f9d51aa6cf945f5536af6be7e387f4572896
Swetha Karthik <swethakarthik@Swethas-MacBook-Air.local>
2026-08-05 16:52:25 -0400

    Fix clean data function

--- preprocessing/clean_data.py
+++ preprocessing/clean_data.py
@@ -1,17 +1,14 @@
 import pandas as pd
-def clean_dataframe(df)
 
-print("MISSING VALUES")
-print(df.isnull().sum())
 
-for column in df.columns:
+def clean_dataframe(df):
+    print("MISSING VALUES")
+    print(df.isnull().sum())
 
+    for column in df.columns:
         if df[column].dtype == "object":
-
             df[column] = df[column].fillna("Unknown")
-
         else:
-
-            df[column] = df[column].fillna(df[column].mean())
+            df[column] = df[column].fillna(df[column].median())
 
     return df
